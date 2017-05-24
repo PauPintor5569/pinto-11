da900684be68284d522726c73e985cd438c1ab8f
PauPintor5569 <pcpinto@uc.cl>
2017-05-24 13:42:32 -0400

    Clase de hoy

---
+++ about.php
@@ -0,0 +1,12 @@
+<?php include('header.php')?>
+
+<div class="row">
+<div class="col-sm-8">
+<h3>Sobre el proyecto</h3>
+<p>Para comenzar a construir mi propio portafolio online, primero debo buscar referentes que traten sobre la temática de lo que he hecho hasta este momento.
+Dentro de los temas en los cuales se realizó la búsqueda, están la ilustración, publicidad, y diseño gráfico. No obstante, también se buscó de otros portafolios de diseño que tuvieran algún aspecto a destacar, tal como la exhibición o dirección de fotografía para enseñar los proyectos.
+La idea es mostrar todos los trabajos que he desarrollado, aunque no está aún claro si serán solamente de la carrera o también incluir trabajos externos.</p>
+</div><!--/col-sm-12-->
+</div><!--/row->
+
+<?php include('footer.php')?>
---
+++ footer.php
@@ -0,0 +1,10 @@
+</div><!--/container-->
+<footer class="footer">
+<div class="container">
+<p class="text-muted"><?php echo $title;?> por Paula Pinto Ramos. 24 de Mayo de 2017.</p>
+</div>
+</footer>
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
